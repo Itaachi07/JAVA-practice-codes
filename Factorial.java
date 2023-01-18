3dff522193c64bab93e68f1bafc77f80103776d3
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-01-18 18:23:08 +0530

    factorial

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial{
+ public static void main(String[] args) {
+        
+    int f=1, n=5;
+    for(int i=n;i>0;i--) 
+    {
+        f=f*i;
+    }
+    System.out.println(f);
+    }
+}
