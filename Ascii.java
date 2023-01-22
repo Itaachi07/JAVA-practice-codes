c61c8f537464005d6081ee809d718f10360f1f87
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-01-22 23:36:02 +0530

    ascii value program2

--- Ascii.java
+++ Ascii.java
@@ -1,10 +1,15 @@
 public class Ascii {
     public static void main(String[] args) {
+        //Assigning a Variable to the int Variable
         int a = 'a';
         System.out.println(a);
-
+        
         char ch = 'b';
         int b= ch;
         System.out.println(ch+" --"+ b);
+
+        //Using typecasting
+        int b1 =(int)ch;
+        System.out.println(b1);
     }
 }
