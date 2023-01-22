62ac0cfd8d0230aced0a2b62df01b69b1a680206
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-01-22 23:25:42 +0530

    ascii value program

---
+++ Ascii.java
@@ -0,0 +1,10 @@
+public class Ascii {
+    public static void main(String[] args) {
+        int a = 'a';
+        System.out.println(a);
+
+        char ch = 'b';
+        int b= ch;
+        System.out.println(ch+" --"+ b);
+    }
+}
