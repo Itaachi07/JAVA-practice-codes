e31e4040321819c08619dd7bc7a3a283b0d89bb5
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-02 22:01:32 +0530

    copy one array to another array

---
+++ ArrayCopy.java
@@ -0,0 +1,16 @@
+public class ArrayCopy {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        int [] arr2 = new int [arr.length] ;
+        
+        for(int i =0;i<arr.length;i++)
+        {
+            arr2 [i] = arr[i];
+        }
+        
+        for(int i =0;i<arr2.length;i++)
+        {
+            System.out.print(arr2[i] +" ");
+        }
+    }
+}
