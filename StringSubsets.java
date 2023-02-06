c1af4932482e725ecc1db3f0c658194bc48cd6e6
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-06 21:54:57 +0530

    All possible substring of a String

---
+++ StringSubsets.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class StringSubsets {
+    public static void main(String[] args) {
+        String str = "fun";
+        int i =0, temp=0;
+        int len = str.length();
+        ArrayList al = new ArrayList<>();
+
+        for(i=0; i<len;i++)
+        {
+            for(int j=i;j<len;j++)
+            {
+                String sub = str.substring(i, j+1);
+                al.add(sub);
+            }
+        }
+        for (Object c  : al) {
+            System.out.println(c);
+        }
+        
+
+    }
+}
