13213552a33e342a4c238f2ad47394c836aed9c4
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-07 23:10:07 +0530

    remove spaces

--- StringRemoveSpace.java
+++ StringRemoveSpace.java
@@ -1,5 +1,7 @@
 public class StringRemoveSpace {
     public static void main(String[] args) {
-        
+       String str = "Hello World , How Are You ?"; 
+       str = str.replaceAll("\\s", "");
+       System.out.println(str);
     }
 }
