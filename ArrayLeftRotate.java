6abdef90032a73cadd68b2a09f345fdada3388ba
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-07 22:36:54 +0530

    rotate array to left

--- ArrayLeftRotate.java
+++ ArrayLeftRotate.java
@@ -1,3 +1,23 @@
 public class ArrayLeftRotate {
+    public static void main(String[] args) {
+        int n =3;
+        int[]arr={1,2,3,4,5,6};
+        int [] temp = new int [arr.length];
+        int j = 0 ; 
+        for (int i=n;i<arr.length;i++)
+        {
+            temp[j] = arr[i];
+            j++;
+        }
+        System.out.println(j);
+        for(int i=0;i<n;i++)
+        {
+            temp[j] = arr[i];
+            j++;
+        }
+        for (int i : temp) {
+            System.out.print(i+" ");
+        }
+    }
     
 }
