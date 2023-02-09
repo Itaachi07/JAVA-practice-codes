175fcc48f3e44c3271ae16f301f0a50b2fb63376
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-09 23:30:10 +0530

    number at even postion

---
+++ ArrayEvenPos.java
@@ -0,0 +1,9 @@
+public class ArrayEvenPos {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,4,5,6,7,8};
+        for(int i=1;i<arr.length;i=i+2)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
