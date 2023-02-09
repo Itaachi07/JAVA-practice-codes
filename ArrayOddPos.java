a47c45f86a743fab9dea93a5500c6c924883405a
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-09 23:33:22 +0530

    numbers ar odd postion

---
+++ ArrayOddPos.java
@@ -0,0 +1,9 @@
+public class ArrayOddPos {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,4,5,6,7,8};
+        for(int i=0;i<arr.length;i=i+2)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
