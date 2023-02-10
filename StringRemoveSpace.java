c3d24d3a5c6f721d5203d532a2c8b50a56aa4906
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-10 23:06:24 +0530

    characters to lower case

--- StringRemoveSpace.java
+++ StringRemoveSpace.java
@@ -1,7 +1,8 @@
 public class StringRemoveSpace {
     public static void main(String[] args) {
        String str = "Hello World , How Are You ?"; 
-       str = str.replaceAll("\\s", "");
+      // str = str.replaceAll("\\s", "");
+      str = str.replaceAll("H", "#");
        System.out.println(str);
     }
 }
