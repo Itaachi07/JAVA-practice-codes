4e909610424845a3d711cabb17f6910e28c69ebc
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-11 19:03:28 +0530

    change space with special character

--- StringRemoveSpace.java
+++ StringRemoveSpace.java
@@ -1,8 +1,12 @@
 public class StringRemoveSpace {
     public static void main(String[] args) {
        String str = "Hello World , How Are You ?"; 
+       String str2 =str;
       str = str.replaceAll("\\s", "");
       //str = str.replaceAll("H", "#");
        System.out.println(str);
+       
+       str2 = str2.replace(' ', '_');
+       System.out.println(str2);
     }
 }
