b2fb30971727648532e151ec8ed3f9bb08b2b7f8
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-12 21:31:33 +0530

    reverse string

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,3 +1,11 @@
 public class StringPalindrome {
-    
+    public static void main(String[] args) {
+        String str = "Hello";
+        StringBuffer sb = new StringBuffer(str);
+        StringBuffer sbr = sb.reverse();
+       System.out.println(sbr);
+        
+
+
+    }
 }
