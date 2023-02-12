ee9c57378fa61bbb5ee97cc4e9f1a8a1d441cf58
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-12 21:44:06 +0530

    String reverse using string buffer

---
+++ StringReverse2.java
@@ -0,0 +1,7 @@
+public class StringReverse2 {
+    public static void main(String[] args) {
+        String str = "Congratulations INDIA";
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println(sb.reverse());
+    }
+}
