30ef20a57a74d9d6a4039e6bc579c8a940e2b08a
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-14 22:33:04 +0530

    sum of array elements complete

---
+++ ArraySum.java
@@ -0,0 +1,11 @@
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5,6,7,8,9,10};
+        int sum =0;
+        for(int i =0;i<arr.length;i++)
+        {
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum of array: "+sum);
+    }
+}
