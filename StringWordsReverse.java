9ce9f5440063a707ae4824058b4f80a4899b7ad0
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-17 20:17:35 +0530

    reverse each word approch 1

--- StringWordsReverse.java
+++ StringWordsReverse.java
@@ -1,5 +1,10 @@
 public class StringWordsReverse {
     public static void main(String[] args) {
-        
+        String str ="Hello World";
+        String[] words = str.split(" ");
+        for(int i = 0;i<words.length;i++)
+        {
+            System.out.println(words[i]);
+        }
     }
 }
