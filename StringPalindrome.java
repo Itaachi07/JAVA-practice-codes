abd1c6a5b779ae6a1bc8da3f8e68f7382dabec86
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-21 14:56:39 +0530

    Plindrome changes

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,11 +1,18 @@
 public class StringPalindrome {
     public static void main(String[] args) {
-        String str = "Hello";
-        StringBuffer sb = new StringBuffer(str);
-        StringBuffer sbr = sb.reverse();
-       System.out.println(sbr);
-        
-
+        String str = "Nitin";
+        str =str.toLowerCase();
+        String sbr = "";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            sbr= sbr + str.charAt(i);
+        }
+       //System.out.println(sbr);
+        if(str.equals(sbr))
+        {
+            System.out.println("Palindrome");
+        }else
+        System.out.println("not palindrome");
 
     }
 }
