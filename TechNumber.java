9b168ce7785279376fa1cc82b69ca6a0cbf833ec
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-22 21:45:22 +0530

    Tech number complete

--- TechNumber.java
+++ TechNumber.java
@@ -1,5 +1,19 @@
 public class TechNumber {
     public static void main(String[] args) {
+        int num =2025;
+        int temp = num;
+        int n1 = temp%100;
+        int n2=temp/100;
+        int s =n1+n2;
+        int s1 = (int)Math.pow(s, 2);
        
+        System.out.println(s1 );
+        if(num == s1)
+        {
+            System.out.println(" Tech number");
+        } else 
+        {
+            System.out.println("not a tech number");
+        }
     }
 }
