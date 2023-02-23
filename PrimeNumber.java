b2206506520a0f6b2f9e8ca0fb7e489f5f6d3806
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-23 23:35:10 +0530

    Prime number complete

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,6 +1,21 @@
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        
+        int num = 10;
+        int count =0;
+        for(int i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                count++;
+            }
+        }
+        if (count==2) {
+            System.out.println("the number is Prime");
+        } 
+        else
+        {
+            System.out.println("Not prime");
+        }
     }
 }
