0f31ca544bfe7caeaed86e1ecc4142bac4067bcf
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-04-22 23:51:50 +0530

    commit

--- MatrixMult.java
+++ MatrixMult.java
@@ -10,16 +10,4 @@ public class MatrixMult {
         {
             for(int j=0;j<3;j++)
             {
-                c[i][j]=0;
-                for(int k=0;k<3;k++)
-                {
-                    c[i][j] = a[i][k]*b[k][j];
-                }
-                System.out.print(c[i][j]+ " ");
-            }
-            System.out.println();
-        }
 
-
-    }
-}
