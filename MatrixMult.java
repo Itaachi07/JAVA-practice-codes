3f577c0add5dc2459afb3156a53f5d6045d306bb
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-04-22 23:52:30 +0530

    commit

--- MatrixMult.java
+++ MatrixMult.java
@@ -1,4 +1,4 @@
-//Matrix multiplication
+// multiplication of two 3*3 matrices
 public class MatrixMult {
     public static void main(String[] args) {
         int a[][]={{1,1,1},{2,2,2},{3,3,3}};    
@@ -11,3 +11,16 @@ public class MatrixMult {
             for(int j=0;j<3;j++)
             {
 
+                c[i][j]=0;
+                for(int k=0;k<3;k++)
+                {
+                    c[i][j] = a[i][k]*b[k][j];
+                }
+                System.out.print(c[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
