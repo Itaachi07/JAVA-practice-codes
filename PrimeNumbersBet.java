2885c32fa00dbe4e70f0acaf12fc608be2ce1503
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-04-24 23:03:02 +0530

    Prime numbers between range revision

--- PrimeNumbersBet.java
+++ PrimeNumbersBet.java
@@ -1,19 +1,35 @@
 public class PrimeNumbersBet {
     public static void main(String[] args) {
-        int n1=10;
-        int n2 =20;
-        int i =0, j=0;
-        for(i=n1;i<=n2;i++)
+        // int n1=10;
+        // int n2 =20;
+        // int i =0, j=0;
+        // for(i=n1;i<=n2;i++)
+        // {
+        //     for(j=2;j<=i;j++)
+        //     {
+        //         if(i%j==0)
+        //         {
+        //             break;
+        //         }
+        //     }
+        //     if (i==j) {
+        //         System.out.print(i+" ");
+        //     }
+        // }
+
+        int n1=20, n2=50;
+        int j=0;
+        for(int i=n1;i<=n2;i++)
         {
-            for(j=2;j<=i;j++)
+            for( j=2;j<=i;j++)
             {
-                if(i%j==0)
-                {
+                if (i%j==0) {
                     break;
-                }
+                }   
+                
             }
             if (i==j) {
-                System.out.print(i+" ");
+                System.out.print(i+ " ");
             }
         }
         
