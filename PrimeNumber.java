4869d34754aed573dc6ed86fb2b37ee7c82b1930
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-04-24 22:57:07 +0530

    Prime numbers revision

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,21 +1,38 @@
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        int num = 10;
-        int count =0;
-        for(int i=1;i<=num;i++)
+        // int num = 10;
+        // int count =0;
+        // for(int i=1;i<=num;i++)
+        // {
+        //     if(num%i==0)
+        //     {
+        //         count++;
+        //     }
+        // }
+        // if (count==2) {
+        //     System.out.println("the number is Prime");
+        // } 
+        // else
+        // {
+        //     System.out.println("Not prime");
+        // }
+
+        int num = 11;
+        int count = 0;
+        for(int i =1; i<= num;i++)
         {
             if(num%i==0)
             {
                 count++;
             }
+
         }
-        if (count==2) {
-            System.out.println("the number is Prime");
-        } 
-        else
+        if(count ==2)
         {
-            System.out.println("Not prime");
-        }
+            System.out.println("Prime");
+        }else
+            System.out.println("Not Prime");
+
     }
 }
