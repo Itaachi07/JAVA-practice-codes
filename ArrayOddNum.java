efb3c5d2163eb56c22d8d1ef09de4a37467d0d61
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-04-25 23:26:21 +0530

    commit

--- ArrayOddNum.java
+++ ArrayOddNum.java
@@ -1,11 +1,14 @@
 public class ArrayOddNum {
     public static void main(String[] args) {
         int [] arr = {2,3,5,354,65,36,5,65,654,35,78,6987,961,684,354,654,564};
-        for(int i =0;i<arr.length;i++)
-        {
-            if (arr[i]%2!=0) {
-                System.out.print(arr[i]+" ");
-            }
-        }
+        // for(int i =0;i<arr.length;i++)
+        // {
+        //     if (arr[i]%2!=0) {
+        //         System.out.print(arr[i]+" ");
+        //     }
+        // }
+
+
+        
     }
 }
