dc4075a22246b9b1955c0409d006ef9fe67284b1
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-04-25 23:31:35 +0530

    Odd number in an array

--- ArrayOddNum.java
+++ ArrayOddNum.java
@@ -8,7 +8,12 @@ public class ArrayOddNum {
         //     }
         // }
 
+        for(int i=0;i<arr.length;i++)
+        {
+            if (arr[i]%2!=0) {
+                System.out.print(arr[i]+" ");
+            }
+        }
 
-        
     }
 }
